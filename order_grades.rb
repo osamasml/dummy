2e42d4453b5f4d60c315f4e161bd13f3ea164274
osamasml <osamasml@yahoo.com>
2014-10-19 14:27:53 -0300

    xx

---
+++ order_grades.rb
@@ -0,0 +1,17 @@
+
+rder_grades ( "ahmed","mohamed","yasmine")
+
+ahmed= [80,40,50]
+mohamed=[40,80,90]
+yasmine=[60,90,95]
+all_array = [80,40,50,40,80,90,60,90,95]
+ all_array.each do |x|
+    array_element = x 
+       for i in 0..8    
+          end   
+           end   
+              a = all_array.length
+                 b = all_array.index.x
+                  end
+                   
+                  end 
